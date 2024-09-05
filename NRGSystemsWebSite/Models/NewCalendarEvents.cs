c582ed48333a2a3e2c5f3f5b21c50d2522abe008
nrg6 <nrg6472@hotmail.com>
2024-09-05 16:10:17 +0100

    Changed the design to new Real Steel

---
+++ NRGSystemsWebSite/Models/NewCalendarEvents.cs
@@ -0,0 +1,15 @@
+﻿namespace NRGSystemsWebSite.Models
+{
+    public class NewCalendarEvents
+    {
+        public int CalendarEventId { get; set; }
+        public int NumberOfEvents { get; set; }
+        public bool IsEmpty { get; set; }
+        public string DateBookingMade { get; set; }
+        public string BookingDate { get; set; }
+        public string StartTime { get; set; }
+        public string Duration { get; set; }
+        public string Goal { get; set; }
+        public int BookingClientId { get; set; }
+    }
+}
